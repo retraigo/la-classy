924957b3cce7ce4e8d4bbd7a6e19674474835f83
NeTT <nett@nett.moe>
2023-07-28 15:11:34 +0530

    update denouse

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { dlopen } from "https://deno.land/x/plug@1.0.2/mod.ts";
 export type { FetchOptions } from "https://deno.land/x/plug@1.0.2/mod.ts";
 
-export { useUnique } from "https://deno.land/x/denouse@v0.0.6/mod.ts";
+export { useUnique } from "https://deno.land/x/denouse@v0.0.12/mod.ts";
