233dda9a28f6cb393e109ab68926c42b15aacf03
NeTT <nett@nett.moe>
2024-01-19 00:06:51 +0530

    update deps

--- examples/binary_iris.ts
+++ examples/binary_iris.ts
@@ -6,11 +6,11 @@ import {
   Matrix,
   // Split the dataset
   useSplit,
-} from "https://deno.land/x/vectorizer@v0.3.3/mod.ts";
+} from "https://deno.land/x/vectorizer@v0.3.4/mod.ts";
 import { SGDSolver } from "../src/api/core/solver/sgd.ts";
 import { binCrossEntropy } from "../src/api/core/loss.ts";
 import { sigmoidActivation, tanhActivation } from "../src/api/core/activation.ts";
-import { ConfusionMatrix } from "../deps.ts";
+import {sigmoid} from "../src/helpers.ts"
 
 // Define classes
 const classes = ["Setosa", "Versicolor"];
@@ -23,7 +23,7 @@ const X = new Matrix<"f64">(Float64Array, [data.length, 4]);
 
 // Get the predictors (x) and targets (y)
 data.forEach((fl, i) => X.setRow(i, fl.slice(0, 4).map(Number)));
-const y = new Matrix(Float64Array.from(data.map((fl) => classes.indexOf(fl[4]))), [data.length]);
+const y = new Matrix<"f64">(Float64Array.from(data.map((fl) => classes.indexOf(fl[4]))), [data.length]);
 
 // Split the dataset for training and testing
 const [[x_train, y_train], [x_test, y_test]] = useSplit(
@@ -37,17 +37,14 @@ const solver = new SGDSolver({
   loss: binCrossEntropy(),
   activation: sigmoidActivation(),
 });
-console.log(x_train, y_train)
-
-solver.train(x_train as Matrix<"f64">, y_train as Matrix<"f64">, { learning_rate: 0.01, epochs: 1000, silent: false });
-
+solver.train(x_train, y_train, { learning_rate: 0.01, epochs: 100, silent: false });
 console.log(`training time: ${performance.now() - time}ms`);
 
 const res = []
-for (const row of (x_test as Matrix<"f64">).rows()) {
-  const out = solver.weights?.dot(new Matrix(row, [1])) || -1
+for (const row of x_test.rows()) {
+  const out = sigmoid(Number(solver.weights?.dot(new Matrix<"f64">(row, [1])) || -1))
   res.push(out)
 }
-const y1 = res.map((x) => (x < 0 ? 0 : 1));
-const cMatrix = new ClassificationReport((y_test as Matrix<"f64">).data, y1);
-console.log("Confusion Matrix: ", cMatrix);
+const y1 = res.map((x) => (x < 0.5 ? 0 : 1));
+const cMatrix = new ClassificationReport(y_test.data, y1);
+console.log(cMatrix)
