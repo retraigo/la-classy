789eadedf940b5338425971b774a3c15c1c24acf
NeTT <nett@nett.moe>
2024-01-20 19:58:58 +0530

    try RMSProp

--- examples/iris.ts
+++ examples/iris.ts
@@ -6,12 +6,14 @@ import {
   Matrix,
   // Split the dataset
   useSplit,
-  CategoricalEncoder
+  CategoricalEncoder,
 } from "https://deno.land/x/vectorizer@v0.3.4/mod.ts";
-import { GradientDescentSolver } from "../src/mod.ts";
-import { softmaxActivation } from "../src/mod.ts";
-import { crossEntropy } from "../src/mod.ts";
-import { adamOptimizer } from "../src/mod.ts";
+import {
+  GradientDescentSolver,
+  rmsPropOptimizer,
+  softmaxActivation,
+  crossEntropy,
+} from "../src/mod.ts";
 
 // Read the training dataset
 const _data = Deno.readTextFileSync("examples/iris.csv");
@@ -25,9 +27,9 @@ data.forEach((fl, i) => X.setRow(i, fl.slice(0, 4).map(Number)));
 
 const y_pre = data.map((fl) => fl[4]);
 
-const encoder = new CategoricalEncoder()
+const encoder = new CategoricalEncoder();
 
-const y = encoder.fit(y_pre).transform<"f64">(y_pre, "f64")
+const y = encoder.fit(y_pre).transform<"f64">(y_pre, "f64");
 
 // Split the dataset for training and testing
 const [[x_train, y_train], [x_test, y_test]] = useSplit(
@@ -40,27 +42,32 @@ const time = performance.now();
 const solver = new GradientDescentSolver({
   loss: crossEntropy(),
   activation: softmaxActivation(),
-  optimizer: adamOptimizer(4, 3)
+  optimizer: rmsPropOptimizer(4, 3),
 });
-solver.train(x_train, y_train, { learning_rate: 0.01, epochs: 300, silent: false, n_batches: 20 });
+solver.train(x_train, y_train, {
+  learning_rate: 0.01,
+  epochs: 300,
+  silent: false,
+  n_batches: 20,
+});
 console.log(`training time: ${performance.now() - time}ms`);
 
-const res = solver.predict(x_test)
+const res = solver.predict(x_test);
 
 let i = 0;
 for (const row of res.rows()) {
-  const max = row.reduce((acc, curr, i, arr) => arr[acc] > curr ? acc : i, 0)
-  const newR = new Array(row.length).fill(0)
-  newR[max] = 1
-  res.setRow(i, newR)
+  const max = row.reduce((acc, curr, i, arr) => (arr[acc] > curr ? acc : i), 0);
+  const newR = new Array(row.length).fill(0);
+  newR[max] = 1;
+  res.setRow(i, newR);
   i += 1;
 }
 
-const y_pred = encoder.untransform(res)
-const y_act = encoder.untransform(y_test)
+const y_pred = encoder.untransform(res);
+const y_act = encoder.untransform(y_test);
 
 for (let i = 0; i < y_pred.length; i += 1) {
-  console.log(y_pred[i], y_act[i])
+  console.log(y_pred[i], y_act[i]);
 }
 
-console.log(new ClassificationReport(y_act, y_pred))
+console.log(new ClassificationReport(y_act, y_pred));
