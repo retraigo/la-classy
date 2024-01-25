00d4bcd0f716b0195488d629c4aba5bbc43f8521
NeTT <nett@nett.moe>
2024-01-25 12:38:37 +0530

    change r2 calculation

--- crates/classy/src/core/solver/ols.rs
+++ crates/classy/src/core/solver/ols.rs
@@ -8,7 +8,11 @@ pub struct OrdinaryLeastSquares;
 
 impl OrdinaryLeastSquares {
     pub fn train(&self, data: &DMatrix<f64>, targets: &DMatrix<f64>, silent: bool) -> DMatrix<f64> {
-        let n = targets.len();
+        let y_mean = targets.row_mean();
+        let mut sst = 0f64;
+        for row in targets.row_iter() {
+            sst += (row - &y_mean).map(|x| x.powi(2)).sum();
+        }
 
         let x_transpose = data.transpose();
 
@@ -19,25 +23,18 @@ impl OrdinaryLeastSquares {
             Some(inv) => inv,
             None => panic!("Unable to invert XtX"),
         };
-        let y_mean = targets.mean();
-
         // OLS Weights = (XtX)' * XtY
-        let weights: DMatrix<f64> = (xtx_inverse * xty).transpose();
+        let weights: DMatrix<f64> = xtx_inverse * xty;
 
         // Calculate R2 only if `silent` is set to false
         if !silent {
-            let mut sse = 0f64;
-            let mut sst = 0f64;
-
-            for i in 0..n {
-                let _sse = targets[i] - weights.dot(&data.row(i));
-                let _sst = targets[i] - y_mean;
-                sse += _sse * _sse;
-                sst += _sst * _sst;
-            }
+            let sse = (targets - (data * &weights))
+                .map(|x| x.powi(2))
+                .sum();
             let r2 = 1f64 - (sse / sst);
             println!("R2 Score: {}", r2);
         }
+
         weights
     }
     pub fn predict(&self, data: &DMatrix<f64>, weights: &DMatrix<f64>) -> DMatrix<f64> {
